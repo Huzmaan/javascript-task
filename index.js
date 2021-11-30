b7151d3f99429f2216e7aae73d1348debe904236
Huzmaan <princehuzmaanpasta@gmail.com>
2021-12-01 01:51:23 +0500

    task

---
+++ index.js
@@ -0,0 +1,34 @@
+// let a ="".length;
+// console.log(a)
+
+// *
+// **
+// ***
+// ****
+// *****
+
+//1
+//2
+//3
+//4
+//5
+
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+//1 2 3 4 5
+// for (var i = 1; i <= 5; i++) {
+//   let a = "";
+//   for (var j = 1; j <= i; j++) {
+//     a +=j + " ";// 1 2
+// }
+// console.log(a);
+// }
+// for(var i = 0; i <5; i++){
+//     console.log("*")
+//     for(var j = i; j <=i;j++){
+//     console.log("* \n")
+//     }
+//   console.log(i)
+// }
